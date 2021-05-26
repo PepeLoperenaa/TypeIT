03bbca7c24cdf2c73c7c14df65c4dd929abe365b
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-05-26 10:00:38 +0100

    added hardcoded statistics methods

--- source_code/TypeIT/TypeIT/Utilities/Statistics.cs
+++ source_code/TypeIT/TypeIT/Utilities/Statistics.cs
@@ -8,5 +8,50 @@ namespace TypeIT.Utilities
 {
     class Statistics
     {
+        double WPM = 0;
+        double average = 0;
+        int mistakes = 0;
+        ConsoleKeyInfo keyInfo;
+        string text = "hi"; //hard coded should be changed later.
+        StringBuilder stringBuilder = new StringBuilder();
+        int totalWordsWritten = 0; //this should be incremented everytime a word is written. Could be wrong
+         
+
+        /*
+         * This class will be hardcoded but will be changed later with the actual documents when parsed.  
+         */
+        private double wordsPerMinute(double minutes, double words) //this will not have input through the method
+        {
+            WPM =  minutes / words;
+            return WPM; //thsi will be stored in the .TypeIT user. 
+        }
+
+        private void mistakesDone()
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                stringBuilder.Append(keyInfo.Key);
+
+                if (keyInfo.Equals(text[i]))
+                {
+                    
+                } else
+                {
+                    mistakes++; //when writing the document, and a mistake is done, this should be added into the .TypeIT user statistics.
+                }
+            }            
+        }
+
+        private double averageSpeed(double time)
+        {
+            average =  WPM / time;
+            return average;  //this will be stored in the .TypeIT user. 
+        }
+
+        private int accuracy()
+        {
+             int accuracy = totalWordsWritten / mistakes;
+            return accuracy; 
+        } 
     }
 }
