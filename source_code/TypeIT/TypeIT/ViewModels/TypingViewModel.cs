463dd994fdbd371912b4b44789fc8199ab8324c6
costa <costandinoh@gmail.com>
2021-06-01 11:02:09 +0200

    created the typing page

---
+++ source_code/TypeIT/TypeIT/ViewModels/TypingViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TypeIT.Commands;
+using TypeIT.Stores;
+
+namespace TypeIT.ViewModels
+{
+    class TypingViewModel : ViewModelBase
+    {
+        public ICommand NavigateHomeCommand { get; }
+        public TypingViewModel(NavigationStore navigationStore)
+        {
+            NavigateHomeCommand = new NavigateCommand<DashboardViewModel>(navigationStore, () => new DashboardViewModel(navigationStore));
+        }
+    }
+}
