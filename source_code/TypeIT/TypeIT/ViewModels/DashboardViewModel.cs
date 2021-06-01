196d151b215f72e568d9e35d2ba5248d61fb9406
costa <costandinoh@gmail.com>
2021-06-01 13:01:27 +0200

    fixed home exit button

--- source_code/TypeIT/TypeIT/ViewModels/DashboardViewModel.cs
+++ source_code/TypeIT/TypeIT/ViewModels/DashboardViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using TypeIT.Commands;
 using TypeIT.Stores;
@@ -31,7 +32,11 @@ namespace TypeIT.ViewModels
 
         private void ClickedExit()
         {
-            System.Windows.Application.Current.Shutdown();
+            if (MessageBox.Show("Are you sure you want to leave us we will never forget this!!!",
+            "Quit", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                System.Windows.Application.Current.Shutdown();
+            }
         }
     }
 }
