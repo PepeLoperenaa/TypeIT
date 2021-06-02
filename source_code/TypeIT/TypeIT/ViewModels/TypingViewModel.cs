1999e7a3bc3afaf100f7f715b4832c48014430e8
AlsoSteffen <52997388+AlsoSteffen@users.noreply.github.com>
2021-06-02 14:32:04 +0200

    Added Text based on directory, added next page after finishing text

--- source_code/TypeIT/TypeIT/ViewModels/TypingViewModel.cs
+++ source_code/TypeIT/TypeIT/ViewModels/TypingViewModel.cs
@@ -1,11 +1,8 @@
-﻿using Prism.Commands;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
 using System.Windows.Input;
 using TypeIT.Commands;
 using TypeIT.Models;
@@ -26,42 +23,38 @@ namespace TypeIT.ViewModels
             }
             set
             {
-                if (InputString == "" && TypingModel.CurrentWordIndex == 0)
+                if (TypingModel.CurrentLetterIndex == 0 && TypingModel.CurrentWordIndex == 0)
                 {
                     TypingModel.StartTime = DateTime.Now;
+                    TypingModel.TypingTimer.Start();
                 }
-                inputString = value;
 
+                inputString = value;
                 TypeWord(TypingModel.CurrentWord);
-                TypingModel.AverageAccuracy = CalculateAccuracy(TypingModel.Text.Length);
-                TypingModel.AverageTypingSpeed = CalculateTypingSpeed(TypingModel.CurrentWordIndex);
             }
         }
 
         public TypingViewModel(NavigationStore navigationStore)
         {
-            TypingModel = new TypingModel(File.ReadAllText("../../../Documents/20.txt"));
-            TypingModel.CurrentWord = GetWord(TypingModel.Text, TypingModel.CurrentWordIndex);
+            TypingModel = new TypingModel("../../../Documents/Overlord 1");
+
+            string FileText = TypingModel.Text;            
+
+            TypingModel.CurrentWord = GetWord(FileText, TypingModel.CurrentWordIndex);
+            TypingModel.TypingTimer.Elapsed += OnTimedEvent;
+            TypingModel.TypingTimer.Interval = 1000;
 
             NavigateHomeCommand = new NavigateCommand<DashboardViewModel>(navigationStore, () => new DashboardViewModel(navigationStore));
         }
 
-        public int CalculateErrorSpace(string word)
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
         {
-            int minSpace = 5;
-            int calculatedSpace = (int)((word.Length * 1.5) / 2);
-
-            return minSpace > calculatedSpace ? minSpace : calculatedSpace;
-        }
-
-        public double CalculateTypingSpeed(int wordNumber)
-        {
-            return Math.Round((wordNumber / (DateTime.Now - TypingModel.StartTime).TotalSeconds) * 60, 2);
-        }
-
-        public double CalculateAccuracy(int totalChars)
-        {
-            return Math.Round(((Double)(totalChars - TypingModel.TotalMistakes) / totalChars) * 100, 2);
+            TypingModel.AverageAccuracy = TypingModel.CalculateAccuracy(TypingModel.CurrentLetterFromText);
+            if (TypingModel.AverageAccuracy < 0)
+            {
+                TypingModel.AverageAccuracy = 0;
+            }
+            TypingModel.AverageTypingSpeed = TypingModel.CalculateTypingSpeed(TypingModel.CurrentWordIndex);
         }
 
         public int GetMistakeIndex(string word)
@@ -75,7 +68,7 @@ namespace TypeIT.ViewModels
                         return i;
                     }
                 }
-                catch (IndexOutOfRangeException ex)
+                catch (IndexOutOfRangeException)
                 {
                     return word.Length;
                 }
@@ -83,41 +76,74 @@ namespace TypeIT.ViewModels
             return -1;
         }
 
+        private bool IsWordCorrect(string word)
+        {
+            if (InputString.Length > 0)
+            {
+                // check if the end of the word is a space to determine when to break to the next word
+                if (InputString.Last() == ' ')
+                {
+                    // check if the word minus the space at the end is equal to the expected word
+                    if (InputString[0..^1].Equals(word))
+                    {
+                        InputString = "";
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
         public void TypeWord(string word)
         {
             TypingModel.CurrentMistakes = 0;
 
-            string text = InputString;
-
             // set the number of letters they can input after making a mistake
-            if (word.Contains(text))
+            if (word.Contains(InputString))
             {
-                TypingModel.ErrorSpace = InputString.Length + CalculateErrorSpace(word);
+                TypingModel.ErrorSpace = InputString.Length + TypingModel.CalculateErrorSpace(word);
             }
 
-            if (text.Length > 0)
+            if (IsWordCorrect(word))
             {
-                if (text.Last() == ' ')
+                if (TypingModel.CurrentWordIndex == GetNumberOfWords(TypingModel.Text))
                 {
-                    if (text[0..^1].Equals(word))
+                    TypingModel.CurrentWordIndex = 0;
+                    TypingModel.CurrentLetterIndex = 0;
+                    if (TypingModel.HasNextPage())
                     {
-                        TypingModel.CurrentWordIndex++;
+                        TypingModel.NextPage();
                         TypingModel.CurrentWord = GetWord(TypingModel.Text, TypingModel.CurrentWordIndex);
-                        InputString = "";
-                        return;
+                    }
+                    else
+                    {
+                        // Go back to home page
                     }
                 }
+                else
+                {
+                    TypingModel.CurrentWordIndex++;
+                    TypingModel.CurrentLetterIndex = 0;
+                    TypingModel.CurrentWord = GetWord(TypingModel.Text, TypingModel.CurrentWordIndex);
+                }
+                return;
             }
 
-            TypingModel.CurrentMistakes = text.Length - GetMistakeIndex(word);
-            TypingModel.TotalMistakes += TypingModel.CurrentMistakes;
+            TypingModel.CurrentLetterIndex = InputString.Length;
+            TypingModel.CurrentLetterFromText++;
+
+            if (GetMistakeIndex(word) >= 0)
+            {
+                TypingModel.CurrentMistakes = InputString.Length - GetMistakeIndex(word);
+                TypingModel.TotalMistakes++;
+            }
         }
 
         public string GetWord(string text, int wordIndex)
         {
             string[] words = text.Split(' ');
 
-            if (wordIndex < GetNumberOfWords(text))
+            if (wordIndex <= GetNumberOfWords(text))
             {
                 return words[wordIndex];
             }
@@ -126,7 +152,8 @@ namespace TypeIT.ViewModels
 
         public int GetNumberOfWords(string text)
         {
-            return text.Split(' ').Length;
+            int number = text.Split(' ').Length - 1;
+            return number;
         }
     }
 }
