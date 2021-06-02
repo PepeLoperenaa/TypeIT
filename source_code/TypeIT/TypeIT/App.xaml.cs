86d139732c591d0a4c91c982ac8b0c1f69d4132c
Chr1ssW <56091550+Chr1ssW@users.noreply.github.com>
2021-06-02 13:13:03 +0200

    Added user creation through application

--- source_code/TypeIT/TypeIT/App.xaml.cs
+++ source_code/TypeIT/TypeIT/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using TypeIT.FileTypes;
 using TypeIT.Stores;
 using TypeIT.ViewModels;
 
@@ -34,6 +35,9 @@ namespace TypeIT
 
             MainWindow.Show();
             base.OnStartup(e);
+
+            NewUserCreation.newUser("hel  lo");
+            NewUserCreation.addingDataIntoAnXml("../../../FileTypes/hello.TypeIT");
         }
     }
 }
--- source_code/TypeIT/TypeIT/FileTypes/NewUserCreation.cs
+++ source_code/TypeIT/TypeIT/FileTypes/NewUserCreation.cs
@@ -3,33 +3,42 @@ using System.Xml.Linq;
 
 namespace TypeIT.FileTypes
 {
-    class NewUserCreation
+    public static class NewUserCreation
     {
-        public void newUser(string name, string fileName)
+        public static void newUser(string name)
         {
             XDocument doc;
-        
-
             doc = new XDocument(new XElement("UserProfile",
-                                       new XElement("Name", name), //after specifying the tag, the value should be added.
-                                       new XElement("Value",
-                                           new XElement("Statistics",
+                                       new XElement("Name", name), //after specifying the tag, the value should be added
+                                       new XElement("Statistics",
+                                           new XElement("HighestWPM"),
+                                           new XElement("AverageWPM"),
+                                           new XElement("AverageAccuracy"),
+                                           new XElement("HoursSpent"),
+                                           new XElement("TypedTypedWordsTotalWords"),
+                                           new XElement("DailyRecords",
+                                               new XElement("Day", 
+                                               new XElement("Date"),
                                                new XElement("WPM"),
-                                               new XElement("Average"),
-                                               new XElement("HoursSpent"),
-                                               new XElement("TypedWords"))),
+                                               new XElement("Average")))),
                                        new XElement("Settings",
                                            new XElement("Theme"),
                                            new XElement("GameMode")),
                                        new XElement("Achievements",
                                            new XElement("Achievement",
-                                               new XElement("AchievementName")))));
+                                               new XElement("AchievementName"))),
+                                       new XElement("Documents",
+                                           new XElement("Document", 
+                                                new XElement("DocumentName"), 
+                                                new XElement("TotalPageNumber"), 
+                                                new XElement("UserPageNumber"), 
+                                                new XElement("DocumentAccuracy")))));
 
-            doc.Save(Directory.GetCurrentDirectory() + "//PepeLoperena.TypeIT");
+            doc.Save("../../../FileTypes/" + name + ".TypeIT");
             //when we save in the current directory, it will save the data in the debug folder.
         }
 
-        public void getElementsFromTags(string filePath, string tag)
+        public static void getElementsFromTags(string filePath, string tag)
         {
             XDocument doc = XDocument.Load(filePath);
             //XDocument parse = XDocument.Parse(fileChris);
@@ -40,17 +49,17 @@ namespace TypeIT.FileTypes
                 //return a list of string. 
             }
         }
-        public void addingDataIntoAnXml(string filePath) //to add information into the element
+        public static void addingDataIntoAnXml(string filePath) //to add information into the element
         {
             //string file = "C:/Users/MyPC/source/repos/XMLChanging/XMLChanging/folder/userFile.TypeIT";
             XDocument doc = XDocument.Load(filePath);
 
             doc.Root.Element("Name").Value = "Example";
-            doc.Root.Element("Value").Element("Statistics").Element("WPM").Value = "20";
-            doc.Root.Element("Value").Element("Statistics").Element("Average").Value = "20";
-            doc.Root.Element("Value").Element("Statistics").Element("Accuracy").Value = "20";
-            doc.Root.Element("Value").Element("Statistics").Element("HoursSpent").Value = "20";
-            doc.Root.Element("Value").Element("Statistics").Element("TypedWords").Value = "20";
+            doc.Root.Element("Statistics").Element("HighestWPM").Value = "20";
+            doc.Root.Element("Statistics").Element("AverageWPM").Value = "20";
+            doc.Root.Element("Statistics").Element("AverageAccuracy").Value = "20";
+            doc.Root.Element("Statistics").Element("HoursSpent").Value = "20";
+            doc.Root.Element("Statistics").Element("TypedTypedWordsTotalWords").Value = "20";
             doc.Root.Element("Settings").Element("Theme").Value = "LightMode";
             doc.Root.Element("Settings").Element("GameMode").Value = "Hard";
             doc.Root.Element("Achievements").Element("Achievement").Element("AchievementName").Value = "Yes";
