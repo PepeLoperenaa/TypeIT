365456936273afabf31a1a7317a527be00ffba13
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-06-02 10:50:27 +0100

    added method to get value of method

--- source_code/TypeIT/TypeIT/FileTypes/NewUserCreation.cs
+++ source_code/TypeIT/TypeIT/FileTypes/NewUserCreation.cs
@@ -1,36 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Xml.Linq;
 
 namespace TypeIT.FileTypes
 {
     class NewUserCreation
     {
-        public void newUser() {
+        public void newUser(string name)
+        {
             XDocument doc;
-        
 
-        doc = new XDocument(new XElement("UserProfile",
-                                       new XElement("Name", "Pepe Loperena"), //after specifying the tag, the value should be added.
-                                       new XElement("Value",
-                                           new XElement("Statistics",
-                                               new XElement("WPM"),
-                                               new XElement("Average"),
-                                               new XElement("HoursSpent"),
-                                               new XElement("TypedWords"))),
-                                       new XElement("Settings",
-                                           new XElement("Theme"),
-                                           new XElement("GameMode")),
-                                       new XElement("Achievements",
-                                           new XElement("Achievement",
-                                               new XElement("AchievementName")))));
+            doc = new XDocument(new XElement("UserProfile",
+                                           new XElement("Name", "Pepe Loperena"), //after specifying the tag, the value should be added.
+                                           new XElement("Value",
+                                               new XElement("Statistics",
+                                                   new XElement("WPM"), 
+                                                   new XElement("Average"),
+                                                   new XElement("HoursSpent"),
+                                                   new XElement("TypedWords"))),
+                                           new XElement("Settings",
+                                               new XElement("Theme"),
+                                               new XElement("GameMode")),
+                                           new XElement("Achievements",
+                                               new XElement("Achievement",
+                                                   new XElement("AchievementName")))));
 
-            doc.Save(Directory.GetCurrentDirectory() + "//PepeLoperena.TypeIT"); //this may need to be changed to where the user wants to save and what name
+            doc.Save(Directory.GetCurrentDirectory() + "//PepeLoperena.TypeIT");
+            //when we save in the current directory, it will save the data in the debug folder.
+        }
 
+        public void getElementsFromTags(string filePath)
+        {
+            XDocument doc = XDocument.Load(filePath);
+            //XDocument parse = XDocument.Parse(fileChris);
+
+            foreach (XElement lev2 in doc.Descendants("level2"))
+            {
+                string lev2Value = (string)lev2;
+            }
         }
 
     }
