ba99a39fed955b588ad4d02e6b82e9a696663a20
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-06-02 11:35:07 +0100

    changes in  typeit file

--- source_code/TypeIT/TypeIT/FileTypes/NewUserCreation.cs
+++ source_code/TypeIT/TypeIT/FileTypes/NewUserCreation.cs
@@ -5,13 +5,13 @@ namespace TypeIT.FileTypes
 {
     class NewUserCreation
     {
-        public void newUser(string name)
+        public void newUser(string name, string fileName)
         {
             XDocument doc;
         
 
             doc = new XDocument(new XElement("UserProfile",
-                                       new XElement("Name", "Pepe Loperena"), //after specifying the tag, the value should be added.
+                                       new XElement("Name", name), //after specifying the tag, the value should be added.
                                        new XElement("Value",
                                            new XElement("Statistics",
                                                new XElement("WPM"),
@@ -29,20 +29,21 @@ namespace TypeIT.FileTypes
             //when we save in the current directory, it will save the data in the debug folder.
         }
 
-        public void getElementsFromTags(string filePath)
+        public void getElementsFromTags(string filePath, string tag)
         {
             XDocument doc = XDocument.Load(filePath);
             //XDocument parse = XDocument.Parse(fileChris);
 
-            foreach (XElement lev2 in doc.Descendants("level2"))
+            foreach (XElement lev2 in doc.Descendants(tag))
             {
                 string lev2Value = (string)lev2;
+                //return a list of string. 
             }
         }
-        public void addingDataIntoAnXml()
+        public void addingDataIntoAnXml(string filePath) //to add information into the element
         {
-            string file = "C:/Users/MyPC/source/repos/XMLChanging/XMLChanging/folder/userFile.TypeIT";
-            XDocument doc = XDocument.Load(file);
+            //string file = "C:/Users/MyPC/source/repos/XMLChanging/XMLChanging/folder/userFile.TypeIT";
+            XDocument doc = XDocument.Load(filePath);
 
             doc.Root.Element("Name").Value = "Example";
             doc.Root.Element("Value").Element("Statistics").Element("WPM").Value = "20";
@@ -53,7 +54,7 @@ namespace TypeIT.FileTypes
             doc.Root.Element("Settings").Element("Theme").Value = "LightMode";
             doc.Root.Element("Settings").Element("GameMode").Value = "Hard";
             doc.Root.Element("Achievements").Element("Achievement").Element("AchievementName").Value = "Yes";
-            doc.Save(file);
+            doc.Save(filePath);
         }
     }
 }
