5c6e3752a67a73c6f35d0e6debf5d52aa3386fbd
Georgi768 <59054341+Georgi768@users.noreply.github.com>
2021-06-02 11:11:58 +0200

    adding a method for changing the elements of the file

--- source_code/TypeIT/TypeIT/FileTypes/NewUserCreation.cs
+++ source_code/TypeIT/TypeIT/FileTypes/NewUserCreation.cs
@@ -14,7 +14,7 @@ namespace TypeIT.FileTypes
             XDocument doc;
         
 
-        doc = new XDocument(new XElement("UserProfile",
+            doc = new XDocument(new XElement("UserProfile",
                                        new XElement("Name", "Pepe Loperena"), //after specifying the tag, the value should be added.
                                        new XElement("Value",
                                            new XElement("Statistics",
@@ -32,6 +32,21 @@ namespace TypeIT.FileTypes
             doc.Save(Directory.GetCurrentDirectory() + "//PepeLoperena.TypeIT"); //this may need to be changed to where the user wants to save and what name
 
         }
+        public void addingDataIntoAnXml()
+        {
+            string file = "C:/Users/MyPC/source/repos/XMLChanging/XMLChanging/folder/userFile.TypeIT";
+            XDocument doc = XDocument.Load(file);
 
+            doc.Root.Element("Name").Value = "Example";
+            doc.Root.Element("Value").Element("Statistics").Element("WPM").Value = "20";
+            doc.Root.Element("Value").Element("Statistics").Element("Average").Value = "20";
+            doc.Root.Element("Value").Element("Statistics").Element("Accuracy").Value = "20";
+            doc.Root.Element("Value").Element("Statistics").Element("HoursSpent").Value = "20";
+            doc.Root.Element("Value").Element("Statistics").Element("TypedWords").Value = "20";
+            doc.Root.Element("Settings").Element("Theme").Value = "LightMode";
+            doc.Root.Element("Settings").Element("GameMode").Value = "Hard";
+            doc.Root.Element("Achievements").Element("Achievement").Element("AchievementName").Value = "Yes";
+            doc.Save(file);
+        }
     }
 }
