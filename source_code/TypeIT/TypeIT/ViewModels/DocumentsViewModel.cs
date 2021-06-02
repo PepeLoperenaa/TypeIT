30ec37324f9f8be950c2ff060b5d1933e57d9c33
costa <costandinoh@gmail.com>
2021-06-02 13:08:42 +0200

    Added the open file dialogue

--- source_code/TypeIT/TypeIT/ViewModels/DocumentsViewModel.cs
+++ source_code/TypeIT/TypeIT/ViewModels/DocumentsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Prism.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +14,25 @@ namespace TypeIT.ViewModels
     class DocumentsViewModel : ViewModelBase
     {
         public ICommand NavigateHomeCommand { get; }
+        public ICommand UploadDocumentCommand { get; set; }
 
         public DocumentsViewModel(NavigationStore navigationStore)
         {
            NavigateHomeCommand = new NavigateCommand<DashboardViewModel>(navigationStore, () => new DashboardViewModel(navigationStore));
+           UploadDocumentCommand = new DelegateCommand(ClickedUploadDocument);
+
+        }
+        private void ClickedUploadDocument()
+        {
+            //Custom messagebox
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            Nullable<bool> result = openFileDialog.ShowDialog();
+            //if (result == true)
+            //{
+            //    FileNameTextBox.Text = openFileDialog.FileName;
+            //    TextBlock1.Text = System.IO.File.ReadAllText(openFileDialog.FileName);
+            //}
         }
     }
 }
