35e034b645461e03048cbdbdf7fdcb0351c2a000
Zerof8 <abdalrahman.ghname@github.com>
2021-06-22 20:11:40 +0200

    Contact us button working and redirects to mailto url

--- source_code/TypeIT/TypeIT/ViewModels/AboutViewModel.cs
+++ source_code/TypeIT/TypeIT/ViewModels/AboutViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Prism.Commands;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +14,22 @@ namespace TypeIT.ViewModels
     class AboutViewModel : ViewModelBase
     {
         public ICommand NavigateHomeCommand { get; }
+
+        public ICommand ContactCommand { get; set; }
         public UserStore currentUser { get; set; }
 
         public AboutViewModel(NavigationStore navigationStore, UserStore userStore)
         {
            NavigateHomeCommand = new NavigateCommand<DashboardViewModel>(navigationStore, () => new DashboardViewModel(navigationStore, userStore));
            currentUser = userStore;
+
+            ContactCommand = new DelegateCommand(ClickedContactUs);
+        }
+
+        private void ClickedContactUs()
+        {
+            var url = "mailto:rob.smit@nhlstenden.com?subject=TypeIT%20Contact%20us%20form&amp";
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
         }
     }
 }
